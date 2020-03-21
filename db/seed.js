159dfd3090b1ecb6a4df9cbd24e55b569081d47e
Kelm Brueschke <brueschkekelmp@johndeere.com>
2020-03-21 14:00:38 -0500

    Added data to seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,4 @@
+const mongoose = require("./connection");
 const Customer = require("../models/Customer");
 const Equipment = require("../models/Equipment");
 const Product = require("../models/Product");
@@ -6,58 +7,102 @@ const Type = require("../models/Type");
 const User = require("../models/User");
 const Venue = require("../models/Venue");
 
-Author.find({}).remove(() => {
-  Cookbook.find({}).remove(() => {
-    let meera = Author.create({
-      firstName: 'meera',
-      lastName: 'sodha'
-    }).then(author => {
-      Promise.all([
-        Cookbook.create({
-          title: 'made in india',
-          yearPublished: 2014
-        }).then(cookbook => {
-          author.cookbooks.push(cookbook)
-        }),
-        Cookbook.create({
-          title: 'fresh india',
-          yearPublished: 2018
-        }).then(cookbook => {
-          author.cookbooks.push(cookbook)
-        })
-      ]).then(() => {
-        author.save(err => console.log(err))
-      })
-    })
-    let alison = Author.create({
-      firstName: 'alison',
-      lastName: 'roman'
-    }).then(author => {
-      Promise.all([
-        Cookbook.create({
-          title: 'dining in',
-          yearPublished: 1917
-        }).then(cookbook => {
-          author.cookbooks.push(cookbook)
-        })
-      ]).then(() => {
-        author.save(err => console.log(err))
-      })
-    })
-    let kenji = Author.create({
-      firstName: 'j. kengi',
-      lastName: 'lópez-alt'
-    }).then(author => {
-      Promise.all([
-        Cookbook.create({
-          title: 'the food lab',
-          yearPublished: 2015
-        }).then(cookbook => {
-          author.cookbooks.push(cookbook)
-        })
-      ]).then(() => {
-        author.save(err => console.log(err))
-      })
-    })
-  })
+    Type.deleteMany({}).then(() => {
+        Type.collection.insertMany([
+        {
+            show: "Fireworks",
+            description: `A professional 'Fireworks' display refers to a show that is always conducted outdoors and typically consists of all or some of the following: aerial fireworks shells, ground effects incuding multi-shot barrage units (cakes), fireballs, set pieces (lance-work), close proximity mines, comets, gerbs to name a few. Examples of 'Fireworks' shows occur for July 4th Events, Town Celebrations, Weddings, Anniversaries and Corporate Events just to name a few.`,
+            pictures: ["WeddingFireworks1.png", "JC-FWX-Pond2.png"]
+        },
+        {
+            show: "Pyrotechnics",
+            description: `"Pyrotechnics" can be used in two different methods - Indoor Pyrotechinc Effects or Outdoor Close Proximity Effects. Indoor effects would include flames, mines, comets, gerbs (fountains), concussions, airbursts, flash pots, etc. Some of the uses for this type of effect would include sporting events, concerts, stage productions, corporate events, just to name a few. Outdoor Close Proximity Pyrotechnics ..`,
+            pictures: ["Pyro-ISU1.png", "Slipknot-pyro.jpg"]
+        }
+        {
+            show: "Special Effects",
+            description: `"Special Effects" are different effects which are used to enhance a performance, meeting, presentation, celebration or many other special occaisions. They include such things as confetti, streamers, balloons, atmospherics (fog, dry ice, haze, bubbles, snow) and cryo-jets. These items are typyically non-pyrotechnic and can be used indoors or outdoors.`,
+            pictures: ["Kiss-Confetti.png", "Nickelback-SFX.jpg"]
+        }
+    ])
+
+    .then(types => console.log(types))
+    .catch(err => console.log(err));
 })
+
+
+// Show.deleteMany({}).then(() => {
+//     Show.collection.insertMany([
+//     {
+//         businessName: String,
+//         status: ["Prospect", "Scheduled", "Complete", "Cancelled"],
+//         date: Date,
+//         venue: [
+//     {
+//         ref: "Venue",
+//         type: mongoose.Schema.Types.ObjectId
+//     }  
+//     ],
+//   type: [
+//     {
+//         ref: "Type",
+//         type: mongoose.Schema.Types.ObjectId
+//     }  
+//     ],
+//   user: [
+//       {
+//       ref: "User",
+//       type: mongoose.Schema.Types.ObjectId
+//     }
+// ]);
+
+// .then(shows => console.log(shows))
+// .catch(err => console.log(err));
+// })
+
+User.deleteMany({}).then(() => {
+    User.collection.insertMany([
+    {
+        firstName: 'Anthony',
+        lastName: 'Antelman',
+        userName: 'anthonyantelman',
+        password: '12345',
+        address: '6400 NW 86th Street',
+        city: 'Johnston',
+        state: 'IA',
+        zip: '50131',
+        phone: '515.267.4573',
+        email: 'antelmananthonyj@johndeere.com'
+    },    
+    {
+        firstName: 'Kelm',
+        lastName: 'Brueschke',
+        userName: 'kelmbrueschke',
+        password: '54321',
+        address: '4104 83rd Street',
+        city: 'Urbandale',
+        state: 'IA',
+        zip: '50322',
+        phone: '515.321.2761',
+        email: 'kelmbrueschke@gmail.com'
+    },
+    {
+        firstName: '',
+        lastName: '',
+        userName: '',
+        password: '',
+        address: '',
+        city: '',
+        state: '',
+        zip: '',
+        phone: '',
+        email: ''
+    }
+]);
+
+.then(users => console.log(users))
+.catch(err => console.log(err));
+
+
+
+
