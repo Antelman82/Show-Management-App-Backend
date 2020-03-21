8a47fc5ddaaee8477b6d8f39aec9ab2016c623d8
Kelm Brueschke <brueschkekelmp@johndeere.com>
2020-03-21 09:21:49 -0500

    Added Product Model & updates

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,13 @@
 const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
-const Equipment = new Schema({
+const Product = new Schema({
     name: String,
     description: String,
     size: String,
     quantity: Number,
     type: ["Fireworks", "Pyrotechnics", "Special Effects"]
+    
 });
 
-module.exports = mongoose.model('Equipment', Equipment);
+module.exports = mongoose.model('Product', Product);
