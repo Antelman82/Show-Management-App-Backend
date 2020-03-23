eddd4b56ffb902b0f1a122b81a53bfdd55e17447
Kelm Brueschke <brueschkekelmp@johndeere.com>
2020-03-23 12:34:09 -0500

    Added router to Users.js

--- controllers/users.js
+++ controllers/users.js
@@ -6,4 +6,11 @@ router.get("/", (req, res) => {
     User.find().then(users => res.json(users));
 });
 
+router.post("/", (req, res) => {
+    const newUser = req.body;
+    User.create(newUser).then(created => {
+      res.json(created);
+    });
+  });
+
 module.exports = router
