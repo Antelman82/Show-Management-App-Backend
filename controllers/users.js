b470e3d60b491f3a555dcf79bb34d01db6bcf3bc
Kelm Brueschke <brueschkekelmp@johndeere.com>
2020-03-24 08:45:59 -0500

    Added Get Venue Id router

--- controllers/users.js
+++ controllers/users.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
+
+router.get("/:userName", (req, res) => {
+    console.log(req.params)
+    const userName = req.params.userName;
+    User.findOne({ userName: userName }).then(user => {
+      res.json(user);
+    });
+  });
+
 router.get("/", (req, res) => {
     User.find().then(users => res.json(users));
 });
 
-router.get("/:user.userName", (req, res) => {
-    const userName = req.params.user.userName;
-    User.findOne({ userName: user.userName }).then(user => {
-      res.json(user);
-    });
-  });
+
 
 
 router.post("/", (req, res) => {
--- controllers/venues.js
+++ controllers/venues.js
@@ -4,7 +4,17 @@ const Venue = require("../models/Venue");
 
 router.get("/", (req, res) => {
     Venue.find().then(venues => res.json(venues));
-
 });
+
+router.get("/:venue_Id", (req, res) => {
+    console.log(req.params)
+    const venue_Id = req.params.venue_Id;
+    Venue.findOne({ venue_Id: venue_Id }).then(venue => {
+      res.json(venue);
+    });
+  });
+
+
+
 
 module.exports = router
