50701dff5509d9abb4986a6cde95f32547e9ae29
Kelm Brueschke <brueschkekelmp@johndeere.com>
2020-03-26 16:37:25 -0500

    No Changes

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-<<<<<<< HEAD
 // const mongoURI = 'mongodb://localhost/show-management_db'
 let mongoURI = '';
 if (process.env.NODE_ENV === "production") {
@@ -16,17 +15,6 @@ mongoose.connect(mongoURI,
         useFindAndModify: false })
 
 .catch(error => console.log("Connection Failed", error))
-=======
-mongoose
-    .connect('mongodb://localhost/show-management_db', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false 
-    })
-
-    .catch(error => 
-        console.log("Connection Failed", error))
->>>>>>> app-backend
 
 mongoose.Promise = Promise
 
